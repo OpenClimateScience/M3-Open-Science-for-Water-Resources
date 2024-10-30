210b961f4b445a288c174d29ac0741f312affde4
K. Arthur Endsley <endsley@umich.edu>
2024-10-30 14:13:17 -0600

    Added follow-up material on creating a basin-scale water budget

--- notebooks/scripts/subset_HYSETS_watershed_data.py
+++ notebooks/scripts/subset_HYSETS_watershed_data.py
@@ -1,16 +1,24 @@
 import xarray as xr
 
-OUTPUT_FILE = '/home/arthur/Workspace/NTSG/projects/Y2024_TOPS_Training/data/HYSETS_watershed_YellowstoneRiver.nc'
+OUTPUT_FILE = '/home/arthur/Workspace/NTSG/projects/Y2024_TOPS_Training/data/HYSETS_watersheds.nc'
 HYSETS_DATASET = '/home/arthur.endsley/Downloads/TOPS/HYSETS_2020_QC_stations.nc'
-STATION_IDX = 8209 # Corresponding to WatershedID 8210 (index is one less)
+STATION_IDX = [
+    (8753, 'KansasRiver'),
+    (8209, 'YellowstoneRiver'),
+] # Corresponding to WatershedID 8210, 8754 (index is one less)
 
 def main():
     hysets = xr.open_dataset(HYSETS_DATASET)
-    ds = dict()
-    for key in ('discharge', 'pr', 'tasmax', 'tasmin', 'slope', 'elevation'):
-        ds[key] = hysets[key].loc[STATION_IDX]
+    datasets = []
+    for sid, name in STATION_IDX:
+        ds = dict()
+        for key in ('discharge', 'pr', 'tasmax', 'tasmin', 'slope', 'elevation'):
+            ds[key] = hysets[key].loc[sid]
 
-    ds = xr.Dataset(ds)
+        ds = xr.Dataset(ds)
+        datasets.append(ds)
+
+    ds_combined = xr.concat(datasets, dim = 'id')
     ds.to_netcdf(OUTPUT_FILE)
 
 
